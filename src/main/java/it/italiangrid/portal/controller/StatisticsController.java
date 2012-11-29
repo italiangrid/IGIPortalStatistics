55f65068e3e6bc4f11f0748e506ac9efdbc218bf
dmichelotto <diego.michelotto@cnaf.infn.it>
2012-11-29 10:04:56 +0100

    Adjust project

--- src/main/java/it/italiangrid/portal/controller/StatisticsController.java
+++ src/main/java/it/italiangrid/portal/controller/StatisticsController.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
 import javax.portlet.RenderResponse;
